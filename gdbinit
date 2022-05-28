1f6056b378accc6b41d930f89907f3dfb028165b
ravenxrz <zhang.xingrui@foxmail.com>
2022-05-28 13:21:53 +0800

    nvim-gdb persistent bps

--- gdbinit
+++ gdbinit
@@ -4,7 +4,6 @@ set print pretty on
 # 保存gdb历史命令
 set history save on
 
-
 # 开启log
 set logging file ./gdb.log
 set logging on
@@ -14,7 +13,8 @@ set logging on
 # macos 防卡住
 # set startup-with-shell off
 
-# 退出gdb无提示
+# 退出gdb quit
 define hook-quit
     set confirm off
+    save breakpoints bps.txt
 end
--- nvim/lua/user/conf/nvimgdb.lua
+++ nvim/lua/user/conf/nvimgdb.lua
@@ -1,8 +1,8 @@
 -- NOTE: nvimgdb 依赖 gdb with python support
 -- 检查方式为: gdb --config
 -- 输出类似如下:
-  --with-python=/usr
-  --with-python-libdir=/usr/lib
+--with-python=/usr
+--with-python-libdir=/usr/lib
 
 -- 如果不支持python, 需要从源码编译gdb:
 -- 1. 下载gdb源码: https://ftp.gnu.org/gnu/gdb/, 并解压
@@ -14,6 +14,7 @@
 vim.cmd([[
     let g:nvimgdb_disable_start_keymaps = 1
     let g:nvimgdb_use_find_executables = 0
+    let g:nvimgdb_use_cmake_to_find_executables = 0
     let g:nvimgdb_config_override = {
       \ 'key_next': ';n',
       \ 'key_step': ';s',
@@ -24,6 +25,16 @@ vim.cmd([[
   \ }
 ]])
 
-vim.cmd([[
-  nnoremap <silent> <expr> <leader>dd ":GdbStart gdb -q " . input("debugee file path: ") . "\<ESC>"
-]])
+-- vim.cmd([[
+--   nnoremap <silent> <expr> <leader>dd ":GdbStart gdb -q " . input("debugee file path: ") . "\<ESC>"
+-- ]])
+
+_G.StartGdbSession = function()
+  local exec_file = vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+  vim.api.nvim_command(":GdbStart gdb -q " .. exec_file)
+  vim.defer_fn(function()
+    vim.api.nvim_command(":Gdb source bps.txt")
+  end, 300)
+end
+
+vim.api.nvim_set_keymap("n", "<leader>dd", "<cmd>lua StartGdbSession()<cr>", {})
