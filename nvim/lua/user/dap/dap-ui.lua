f437af97948394f5b6baadcbf42a04e11f442bd9
tiezhu <tiezhu@study.com>
2022-07-14 15:26:44 +0800

    nvim-dap-ui:'sidebar' and 'tray' options are deprecated. use 'layouts' instead.

--- nvim/lua/user/dap/dap-ui.lua
+++ nvim/lua/user/dap/dap-ui.lua
@@ -15,6 +15,29 @@ dapui.setup ({
     repl = "r",
     toggle = "t",
   },
+
+  layouts = {
+    {
+      elements = {
+        'scopes',
+        'breakpoints',
+        'stacks',
+        'watches',
+      },
+      size = 40,
+      position = 'left',
+    },
+    {
+      elements = {
+        'repl',
+        'console',
+      },
+      size = 10,
+      position = 'bottom',
+    },
+  },
+
+--[[
   sidebar = {
     -- You can change the order of elements in the sidebar
     elements = {
@@ -35,6 +58,8 @@ dapui.setup ({
     size = 5,
     position = "bottom", -- Can be "left", "right", "top", "bottom"
   },
+--]]
+
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
     max_width = nil, -- Floats will be treated as percentage of your screen.
